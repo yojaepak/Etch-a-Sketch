f54ed2e89b2c10bb4f5801b672add17247f9036b
Young Park <yjp.youngpark@gmail.com>
2022-06-24 12:16:37 -0400

    Change color of divs on hover
    Will create a trail of divs changing their background color on hover.
    Creating a "pen-like" effect to draw across the grid.

--- sketch.js
+++ sketch.js
@@ -7,7 +7,19 @@ for (let i = 0; i < (gridSize * gridSize); i++) {
     let grid = document.createElement('div');
     grid.id = "box";
     grid.textContent = "box";
+    grid.style.border = "solid";
+    grid.style.borderColor = "blue";
+    grid.style.width = "50px";
+
     container.appendChild(grid);
 }
 
 container.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
+
+const boxes = document.querySelectorAll('#box');
+
+boxes.forEach((grid) =>  {
+    grid.addEventListener('mouseover', function(event) {
+        event.target.style.backgroundColor = "red";
+    })
+})
