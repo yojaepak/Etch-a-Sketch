178107c61764e865dc75bfb9d3c2efe5adff1fbd
Young Park <yjp.youngpark@gmail.com>
2022-06-24 12:59:01 -0400

    Change grid size
    Button added on top of page which will prompt user to enter a new
    grid size between 0 and 100. Putting in a number out of bounds will
    re-prompt user to put a new number. By putting in a valid input the
    grid will change size.

--- sketch.js
+++ sketch.js
@@ -3,22 +3,40 @@ let gridSize = 16;
 
 const container = document.querySelector('#container');
 
-for (let i = 0; i < (gridSize * gridSize); i++) {
-    let grid = document.createElement('div');
-    grid.id = "box";
-    grid.style.border = "solid";
-    grid.style.borderColor = "blue";
+function setGrid(gridSize) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
     
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        let grid = document.createElement('div');
+        grid.id = "box";
+        grid.style.border = "solid";
+        grid.style.borderColor = "blue";
+        
 
-    container.appendChild(grid);
+        container.appendChild(grid);
+    }
+    container.style.gridTemplateColumns = `repeat(${gridSize}, 25px)`;
+
+    const boxes = document.querySelectorAll('#box');
+
+    boxes.forEach((grid) =>  {
+        grid.addEventListener('mouseover', function(event) {
+            event.target.style.backgroundColor = "red";
+        });
+    });
 }
 
-container.style.gridTemplateColumns = `repeat(${gridSize}, 25px)`;
 
-const boxes = document.querySelectorAll('#box');
 
-boxes.forEach((grid) =>  {
-    grid.addEventListener('mouseover', function(event) {
-        event.target.style.backgroundColor = "red";
-    })
-})
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', function(event) {
+    let newSize = prompt("Please enter a new grid size between 0 and 100.");
+    while (newSize < 0 || newSize > 100) {
+        newSize = prompt("Number is out of bounds, please enter a number between 0 and 100.");
+    }
+    setGrid(newSize);
+});
